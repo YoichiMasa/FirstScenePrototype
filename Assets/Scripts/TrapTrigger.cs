786e3e7b1bc044b8ed94d39b33ac1f3f90d31695
YoichiMasa <laurence.jamail@gmail.com>
2014-11-26 00:49:50 -0500

    Implemented Triggers
    I added triggers that I called TrapTriggers. These triggers, right now,
    adds an upward force to the player. There is one that fills the room and
    another next to a ledge.

---
+++ Assets/Scripts/TrapTrigger.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTrigger : MonoBehaviour {
+
+	Rigidbody player;
+	PlayerMovement obj;
+	Transform trap;
+	Vector3 doom;
+	bool trapTriggered = false;
+	bool force = false;
+	bool waiting = false;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody>();
+		trap = GameObject.FindGameObjectWithTag ("Trap").GetComponent<Transform> ();
+		obj = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerMovement> ();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.transform.tag == "Player")
+		{
+			trapTriggered = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if(col.transform.tag == "Player")
+		{
+			trapTriggered = false;
+		}
+	}
+
+	void Update()
+	{
+		if(trapTriggered == true)
+		{
+			force = true;
+			if(force == true)
+			{
+				if(player.velocity.magnitude < 6f)
+				{
+					player.AddForce(Vector3.up*15);
+				}	
+			}
+			if(Input.anyKeyDown && !waiting)
+			{
+				force = false;
+				Wait (3);
+			}
+		}
+	}
+
+	IEnumerator Wait(float sec)
+	{
+		waiting = true;
+		yield return new WaitForSeconds(sec);
+		force = true;
+	}
+}
